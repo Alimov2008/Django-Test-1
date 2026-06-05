97dc786ae15144288f616445598b991859d4d11d
Ali <alimovmuhammadali01222008@gmail.com>
2026-06-05 18:27:39 +0500

    created test post list

--- wblog/urls.py
+++ wblog/urls.py
@@ -2,4 +2,7 @@ from django.urls import path
 
 from .views import *
 
-urlpatterns = [path("", HomeView.as_view(), name="home_page")]
+urlpatterns = [
+    path("", HomeView.as_view(), name="home_page"),
+    path("post_list/", PostView.as_view(), name="post_list"),
+]
--- wblog/views.py
+++ wblog/views.py
@@ -1,8 +1,14 @@
 from django.urls import reverse_lazy
-from django.views.generic import TemplateView
+from django.views.generic import ListView, TemplateView
 
 from .models import Post
 
 
 class HomeView(TemplateView):
     template_name = "wblog/home.html"
+
+
+class PostView(ListView):
+    model = Post
+    template_name = "wblog/post_view.html"
+    context_object_name = "posts"
