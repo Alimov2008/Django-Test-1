efc7a837adcd1fe4e4ba393e2dab0bc8edecc4cf
Ali <alimovmuhammadali01222008@gmail.com>
2026-06-06 23:43:09 +0500

    added post creating page

--- wblog/urls.py
+++ wblog/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path("", HomeView.as_view(), name="home_page"),
     path("post_list/", PostView.as_view(), name="post_list"),
     path("post_list/<int:pk>", PostDetailView.as_view(), name="post_detail_view"),
+    path("post_create/", PostCreateView.as_view(), name="post_create"),
 ]
--- wblog/views.py
+++ wblog/views.py
@@ -1,5 +1,14 @@
+from django.forms.models import BaseModelForm
+from django.http import HttpResponse
 from django.urls import reverse_lazy
-from django.views.generic import DetailView, ListView, TemplateView
+from django.views.generic import (
+    CreateView,
+    DeleteView,
+    DetailView,
+    ListView,
+    TemplateView,
+    UpdateView,
+)
 
 from .models import Post
 
@@ -17,3 +26,19 @@ class PostView(ListView):
 class PostDetailView(DetailView):
     model = Post
     template_name = "wblog/post_detail_view.html"
+
+
+class PostCreateView(CreateView):
+    model = Post
+    template_name = "wblog/post_create_view.html"
+    fields = ["title", "context"]
+    success_url = reverse_lazy("post_list")
+
+    def form_valid(self, form: BaseModelForm) -> HttpResponse:
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["page_title"] = "Create New Post"
+        return context
